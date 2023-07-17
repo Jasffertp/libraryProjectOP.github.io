fbe96e450549799b1fa2fbf440511f212af49ced
JasfferTP <jasfferp@gmail.com>
2023-07-17 22:41:04 +0800

    First Commit with starting page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # libraryProjectOP.github.io
+
+Library Project that displays the books that the user has read or want to read
---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("hello, mom!");
+
+let myLibrary = [];
+
+const book = function () {};
+
+const addBookToLibrary = function () {};
